3876e5f62f545fa00db68e8512e29c4f5d8768b8
networkSorcerer <gigattfyhig@gmail.com>
2024-10-07 14:25:32 +0900

    Arrays

---
+++ src/Arrays연습문제/ArraysExMain.java
@@ -0,0 +1,27 @@
+package Arrays연습문제;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+// 임의의 10개의 과일 이름 입력 후 길이순으로 정렬하고
+// 길이가 같은 경우 사전순 정렬
+public class ArraysExMain {
+    public static void main(String[] args) {
+        String [] fruit = {"Kiwi", "Orange", "Apple", "Banana", "Melon",
+        "Mango","Watermelon", "Cherry", "Plum", "Peach"};
+
+        Arrays.sort(fruit, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if(o1.length() > o2.length()) return 1;
+                else {
+                    if(o1.length() == o2.length()){
+                        return o1.compareTo(o2); // 사전순 정렬, 앞에 값이 큰 경우 양수 반환
+                    }
+                    return -1; // 현상태 유지 
+                }
+
+            }
+        });
+    }
+}
---
+++ src/Wrapper클래스/WrapClassEx.java
@@ -0,0 +1,55 @@
+package Wrapper클래스;
+// Wrapper 클래스란?
+// 기본 타입의 데이터를 객체 타입으로 취급하도록 변환해주는 클래스
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class WrapClassEx {
+    public static void main(String[] args) {
+//        IntegerEx integerEx = new IntegerEx(100);
+//
+//        System.out.println(integerEx.x);
+
+        // Boxing : 기본 타입을 참조 내에 넣는 것
+//        Integer x = 100; // Auto Boxing
+//        int xx = x;  // unboxing
+        sortAndSearchFunc();
+        sortComparatorFunc();
+
+    }
+
+    static void sortAndSearchFunc() {
+        Integer[] array = {5,4,7,9,13,3,1,88,23};
+        //Arrays.sort(array);  // 해당 값을 오름 차순 정렬
+        Arrays.sort(array, Collections.reverseOrder());
+        for (int e : array) System.out.print(e + " ");
+        System.out.println();
+        System.out.println("결과에 대한 인덱스 : " +Arrays.binarySearch(array, 88));
+    }
+
+    // 정렬 오버라이딩 : 상속 받은 메서드를 재정의
+    // 과일 이름을 정렬하는 길이가 짧은 순으로 정렬하고 길이가 같은 경우
+    // 사전 정렬 해주세요
+    // Comparator 클래스의 compare메서드를 오버라이딩해서 정렬 조건 만듦
+
+    static void sortComparatorFunc() {
+        Integer[] array = {5,4,7,9,13,3,3,1,88,23,23};
+        Arrays.sort(array, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (o1 > o2) return 1; // return 1 이라는 건
+                return -1;
+            }
+        });
+        for (int e : array) System.out.println(e  + " ");
+    }
+}
+
+//class IntegerEx {
+//    int x;
+//    IntegerEx(int x ) {
+//        this.x = x;
+//    }
+//}
