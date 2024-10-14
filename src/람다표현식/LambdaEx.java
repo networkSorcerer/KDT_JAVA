b9eb98086793b0582eb0210274ab24dd4751611e
networkSorcerer <gigattfyhig@gmail.com>
2024-10-14 11:12:06 +0900

    IO

---
+++ src/람다표현식/LambdaEx.java
@@ -0,0 +1,41 @@
+package 람다표현식;
+// 람다란? 자바 8 버전에서 도입, 간단한 익명 함수를 생성하기 위한 방법을 제공
+// 람다 표현식은 주로 함수형 프로그램밍 스타일을 지원
+// (매개변수 목록)-> {구현부;}
+// 타입 추론이 가능한 모든 것을 생략 함
+// 매개변수가 하나인 역우는 ()를 생략할수 있음
+// 구현부(함수의 몸체)가 하나의 명령문인 경우 {}로 생략 가능
+
+// 함수형 인터페이스 : 자바의 경우는 함수가 단독으로 존재 할수 없다.
+// 자바에서 함수형 인터페이스를 만들기 위해서는 인터페이스를 만들고
+// 함수를 만들어야 함
+
+interface Calculator {
+    int sum(int a , int b); // 자동으로 public abstract 가 추가됨
+}
+class MyCalculator implements Calculator {
+
+    @Override
+    public int sum(int a, int b) {
+        return a+b;
+    }
+}
+//import java.util.function.BiFunction;
+
+public class LambdaEx {
+    public static void main(String[] args) {
+
+        // 객체 지향 문법을 사용해서 구현
+        // MyCalculator 참조변소로 객체를 접근
+        MyCalculator calc = new MyCalculator();
+        // 인터페이스에서 상속받은 메서드인 sum 을 오버라이딩된 메서드를 호출
+        int rst = calc.sum(3,4);
+        System.out.println(rst);
+
+        // 람다 식으로 구현
+        Calculator mc = (int a, int b) -> a+b; // 인터페이스를 상속 받아 클래스 대신에 람다식으로 구현
+        int rst2 = mc.sum(30,40);
+        System.out.println(rst2);
+
+    }
+}
--- src/자바파이널과제/JavaFinalReportMain.java
+++ src/자바파이널과제/JavaFinalReportMain.java
@@ -30,10 +30,16 @@ public class JavaFinalReportMain {
             String[] splitLine = line.split(" "); // 문자열을 공백 기준으로 잘라 문자열 배열을 생성
             // 생성자를 통해서 Student 객체를 리스트에 추가
             // 생성자의 매개변수로 전달되는 내용은 파일에서 읽어 들인 각 라인의 정보
+            // 국어, 영어, 수학 성적은 연산이 필요하므로 정수로 형 변환
             list.add(new Student(splitLine[0], Integer.parseInt(splitLine[1]),
                     Integer.parseInt(splitLine[2]), Integer.parseInt(splitLine[3])));
         }
+        // 리스트를 정렬할 때 사용하는 메서드
+        // 리스트의 객체를 정렬하는 경우 Comparable 를 상속 받아 compareTo 구현 해줘야 함
         Collections.sort(list);
+        // 향상된 for 문으로 리스트를 순회하면서 요소를 출력
+        // 해당 요소의 객체가 toString 을 오버라이딩 하는 경우 메서드 접근 없이 toString() 내용 출력
+
         for (Student e : list) System.out.println(e);
     }
 }
@@ -57,8 +63,10 @@ class Student implements Comparable<Student>{
     @Override
     public int compareTo(Student o) {
         if(this.getTotalScore() != o.getTotalScore()) {
+            // 결과가 양수이면 정렬 조건이 됨
             return o.getTotalScore() - this.getTotalScore();
         } else {
+            // 총점이 같은 경우는 사전순 정렬을 하기 위해
             return this.name.compareTo(o.name);
         }
     }
